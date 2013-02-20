3a82684eba3e934bdc9faacd7ff23e5a9ccef715
Nathan Yergler <nathan@yergler.net>
2013-02-19 19:25:21 -0800

    Create contact view.

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -6,4 +6,6 @@ import contacts.views
 urlpatterns = patterns('',
     url(r'^$', contacts.views.ListContactView.as_view(),
         name='contacts-list',),
+    url(r'^new$', contacts.views.CreateContactView.as_view(),
+        name='contacts-new',),
 )
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView
+from django.core.urlresolvers import reverse
+from django.views.generic import (
+    CreateView,
+    ListView,
+)
 
 from contacts.models import Contact
 
@@ -7,3 +11,12 @@ class ListContactView(ListView):
 
     model = Contact
     template_name = 'contact_list.html'
+
+
+class CreateContactView(CreateView):
+
+    model = Contact
+    template_name = 'edit_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
