4cce16dc4c4e8fd6c2dad4acd136d65e395873ab
Nathan Yergler <nathan@yergler.net>
2013-02-19 18:44:02 -0800

    Simple contact model and DB settings.

--- addressbook/settings.py
+++ addressbook/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'address.db',
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'contacts',
 )
 
 # A sample logging configuration. The only tangible logging
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    first_name = models.CharField(
+        max_length=255,
+    )
+    last_name = models.CharField(
+        max_length=255,
+
+    )
+
+    email = models.EmailField()
+
+    def __str__(self):
+
+        return ' '.join([
+            self.first_name,
+            self.last_name,
+        ])
