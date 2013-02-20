f8a00e12f3b4e434ff016c5ee840c33134f278dd
Nathan Yergler <nathan@yergler.net>
2013-02-19 19:41:47 -0800

    Tut Checkpoint: view_tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -7,6 +7,10 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from contacts.models import Contact
+from django.test.client import Client
+from django.test.client import RequestFactory
+
+from contacts.views import ListContactView
 
 
 class SimpleTest(TestCase):
@@ -28,3 +32,31 @@ class ContactTests(TestCase):
             str(contact),
             'John Smith',
         )
+
+
+class ContactListViewTests(TestCase):
+    """Contact list view tests."""
+
+    def test_contacts_in_the_context(self):
+
+        client = Client()
+        response = client.get('/')
+
+        self.assertEquals(list(response.context['object_list']), [])
+
+        Contact.objects.create(first_name='foo', last_name='bar')
+        response = client.get('/')
+        self.assertEquals(response.context['object_list'].count(), 1)
+
+    def test_contacts_in_the_context_request_factory(self):
+
+        factory = RequestFactory()
+        request = factory.get('/')
+
+        response = ListContactView.as_view()(request)
+
+        self.assertEquals(list(response.context_data['object_list']), [])
+
+        Contact.objects.create(first_name='foo', last_name='bar')
+        response = ListContactView.as_view()(request)
+        self.assertEquals(response.context_data['object_list'].count(), 1)
