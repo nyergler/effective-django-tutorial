24b2020e40ab54de857caab29d126d837243e6ea
Nathan Yergler <nathan@yergler.net>
2013-02-19 19:00:52 -0800

    Tut Checkpoint: list_contact_view

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+import contacts.views
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'addressbook.views.home', name='home'),
-    # url(r'^addressbook/', include('addressbook.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', contacts.views.ListContactView.as_view(),
+        name='contacts-list',),
 )
--- contacts/views.py
+++ contacts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from contacts.models import Contact
+
+
+class ListContactView(ListView):
+
+    model = Contact
+    template_name = 'contact_list.html'
