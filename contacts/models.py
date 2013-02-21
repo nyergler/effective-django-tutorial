2728afad05a0726ea2fd5686c7947099292d99eb
Nathan Yergler <nathan@yergler.net>
2013-02-21 11:58:33 -0800

    Added address model and show on detail.

--- contacts/models.py
+++ contacts/models.py
@@ -24,3 +24,27 @@ class Contact(models.Model):
     def get_absolute_url(self):
 
         return reverse('contacts-view', kwargs={'pk': self.id})
+
+
+class Address(models.Model):
+
+    contact = models.ForeignKey(Contact)
+    address_type = models.CharField(
+        max_length=10,
+    )
+
+    address = models.CharField(
+        max_length=255,
+    )
+    city = models.CharField(
+        max_length=255,
+    )
+    state = models.CharField(
+        max_length=2,
+    )
+    postal_code = models.CharField(
+        max_length=20,
+    )
+
+    class Meta:
+        unique_together = ('contact', 'address_type',)
