2c2d9ce7bd7ef11df07ec84ee5039df86e4506d5
Nathan Yergler <nathan@yergler.net>
2013-03-04 20:30:29 -0800

    Require confirmation of email when editing a Contact.

---
+++ contacts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from contacts.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+
+    confirm_email = forms.EmailField(
+        "Confirm email",
+        required=True,
+    )
+
+    class Meta:
+        model = Contact
+
+    def __init__(self, *args, **kwargs):
+
+        email = kwargs['instance'].email
+        kwargs.setdefault('initial', {})['confirm_email'] = email
+
+        return super(ContactForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+
+        if (self.cleaned_data.get('email') !=
+            self.cleaned_data.get('confirm_email')):
+
+            raise ValidationError(
+                "Email addresses must match."
+            )
+
+        return self.cleaned_data
--- contacts/views.py
+++ contacts/views.py
@@ -26,6 +26,7 @@ class CreateContactView(CreateView):
 
     model = Contact
     template_name = 'edit_contact.html'
+    form_class = forms.ContactForm
 
     def get_success_url(self):
         return reverse('contacts-list')
@@ -42,6 +43,7 @@ class UpdateContactView(UpdateView):
 
     model = Contact
     template_name = 'edit_contact.html'
+    form_class = forms.ContactForm
 
     def get_success_url(self):
         return reverse('contacts-list')
