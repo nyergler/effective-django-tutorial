df7584607bcc29159d2736a5286a8267bb93905f
Nathan Yergler <nathan@yergler.net>
2017-04-18 18:48:15 -0700

    Splitting unit tests and integration tests.

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,13 +1,9 @@
 from django.test import TestCase
-from django.test import LiveServerTestCase
 from django.test.client import Client
 from django.test.client import RequestFactory
 
-from selenium.webdriver.firefox.webdriver import WebDriver
-
 from .models import Contact
 from .views import ListContactView
-
 
 
 class ContactTests(TestCase):
@@ -49,50 +45,3 @@ class ContactListViewTests(TestCase):
         Contact.objects.create(first_name='foo', last_name='bar')
         response = ListContactView.as_view()(request)
         self.assertEquals(response.context_data['object_list'].count(), 1)
-
-
-class ContactListIntegrationTests(LiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.selenium = WebDriver()
-        super(ContactListIntegrationTests, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super(ContactListIntegrationTests, cls).tearDownClass()
-
-    def test_contact_listed(self):
-
-        # create a test contact
-        Contact.objects.create(first_name='foo', last_name='bar')
-
-        # make sure it's listed as <first> <last> on the list
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        self.assertEqual(
-            self.selenium.find_elements_by_css_selector('.contact')[0].text,
-            'foo bar'
-        )
-
-    def test_add_contact_linked(self):
-
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        self.assert_(
-            self.selenium.find_element_by_link_text('add contact')
-        )
-
-    def test_add_contact(self):
-
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        self.selenium.find_element_by_link_text('add contact').click()
-
-        self.selenium.find_element_by_id('id_first_name').send_keys('test')
-        self.selenium.find_element_by_id('id_last_name').send_keys('contact')
-        self.selenium.find_element_by_id('id_email').send_keys('test@example.com')
-
-        self.selenium.find_element_by_id("save_contact").click()
-        self.assertEqual(
-            self.selenium.find_elements_by_css_selector('.contact')[-1].text,
-            'test contact'
-        )
